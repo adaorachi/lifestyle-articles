1294b0a5a30a671eef551d9db7df26840a03c9fb
MaryAnn Chukwuka <annychuks07@gmail.com>
2020-02-08 12:21:50 +0100

    install bootstrap and its dependencies

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'home_page/home'
+  root to: 'home_page#home'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
