849900b4229a55ec7c354ec0d3b86ef60c9478aa
MaryAnn Chukwuka <annychuks07@gmail.com>
2020-02-14 22:15:09 +0100

    fix homepage image bug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,12 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
+require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("bootstrap/dist/js/bootstrap")
 
-//= require ckeditor/init
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -21,11 +22,7 @@ require("bootstrap/dist/js/bootstrap")
 //= require froala_editor.min.js
 
 
-new FroalaEditor('selector',{
-  
-});
-
-$(document).ready(function(){
+$(document).on('turbolinks:load', function(){
 
 	$('.menu-toggler').on('click', function () {
       $('.navbar-content').toggleClass('open');
