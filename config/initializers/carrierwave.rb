e5f7e3a028cfcf020d9d09d52dd3f8f37b4fda15
MaryAnn Chukwuka <annychuks07@gmail.com>
2020-02-15 02:44:12 +0100

    fix bug in carrierwave/aws config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,13 @@
 CarrierWave.configure do |config|
-  if Rails.env.staging? || Rails.env.production?
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      :provider => 'AWS',
-      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region => 'US East (Ohio)'
-    }
-    config.fog_directory = ENV['S3_BUCKET_NAME']
-    config.storage = :fog
-  else
-    config.storage = :file
-    config.enable_processing = Rails.env.development?
-  end
-end
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     'AKIAIY2DAHBFK2LMKR7A',       # required unless using use_iam_profile
+    aws_secret_access_key: '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',  # required unless using use_iam_profile
+    use_iam_profile:       true,                         # optional, defaults to false
+    region:                'us-east-2'                  # optional, defaults to 'us-east-1'
 
+  }
+  config.fog_directory  = 'sleezy-app' # required
+  config.fog_public     = false  # optional, defaults to true
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
+end
