3ecd1aac36b1db4eda38661ed31fdb05c45a4777
MaryAnn Chukwuka <annychuks07@gmail.com>
2020-02-15 02:31:41 +0100

    fix bug in carrierwave/aws config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws' 
     config.fog_credentials = {
       :provider => 'AWS',
-      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :aws_access_key_id => ENV[AWS_ACCESS_KEY_ID],
+      :aws_secret_access_key => ENV[AWS_SECRET_ACCESS_KEY],
       :region => 'US East (Ohio)'
     }
     config.fog_directory = ENV['S3_BUCKET_NAME']
