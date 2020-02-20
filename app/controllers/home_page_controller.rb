c8bf935b0218d0d1d73ec1e27c4cdf6f3358d500
MaryAnn Chukwuka <annychuks07@gmail.com>
2020-02-20 08:01:10 +0100

    fix bug in homepage view

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -3,7 +3,7 @@ class HomePageController < ApplicationController
     @articles = Article.all_published_articles
     @article_limit_two = @articles.take(2)
     @featured_article_h = Article.featured_article
-    @featured_article = Article.featured_article.first
+    @featured_article = Article.featured_article.last
     @category_first_article = Article.category_all_article
     @categories = Category.all_categories
     @most_popular_articles = Article.most_popular_articles
